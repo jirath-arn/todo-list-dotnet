f389263cf02ab8bbef121933292e415eda64fa67
Jirxth A <jirath.arnamnath@krungsri.com>
2025-10-21 18:00:39 +0700

    Updated swagger

--- obj/Debug/net9.0/todo-list-dotnet.AssemblyInfo.cs
+++ obj/Debug/net9.0/todo-list-dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("todo-list-dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43061f8b87a227db13a5d2a3b77af3f0851c4a54")]
 [assembly: System.Reflection.AssemblyProductAttribute("todo-list-dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("todo-list-dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
